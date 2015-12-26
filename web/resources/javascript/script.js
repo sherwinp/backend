9fae231ce2337f8b31f58f94535d3c249582d4c8
sherwinp <sherwinprice@hotmail.com>
2015-12-26 13:22:01 -0500

    add websocke test.

--- web/resources/javascript/script.js
+++ web/resources/javascript/script.js
@@ -4,9 +4,25 @@
  */
 
 $(document).ready( function() {
-    $('#filetree').fileTree({ root:'/', script:'FileSystemObject' }, function(file) {
-        openFile(file);
-    });
+    
+	 var ws = new WebSocket("ws://localhost:8080/backend/service/messages");
+     ws.onopen = function()
+     {
+        // Web Socket is connected, send data using send()
+        ws.send("Message to send");
+        console.log("Message is sent...");
+     };
+		
+     ws.onmessage = function (evt) 
+     { 
+        var received_msg = evt.data;
+        console.log("Message is received...");
+     };
+		
+     ws.onclose = function()
+     { 
+    	 console.log("Connection is closed..."); 
+     };
 });
 
 function openFile(file) {
